e5001e36b3d45d86d7a93e36faba4f9d26517294
mihainicolaescu <mihaita_nicolaescu@yahoo.com>
2023-09-23 01:17:04 +0300

    First mini project with React :d

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  React  from "react";
+import { createRoot } from "react-dom/client";
+import "./style/avatar.css";
+import "./style/main.css";
+import "./style/description.css";
+import "./style/skills.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App(){
+    return (
+    <div className="container">
+        <h1>Boss Resume</h1>
+        <Avatar />
+        <Description />
+        <Skills />
+    </div>
+    )
+}
+
+function Avatar(){
+    return(
+        <div >
+            <img className="avatar-img" src="images/avatar.jpg" alt="Avatar"></img>
+        </div>
+    )
+}
+
+function Description(){
+    return (
+        <div className="description">
+            <p className="description-text">Meet [Mihaita Boss] 🚀, a seasoned PHP dev on an epic journey to conquer React ⚛️. With PHP prowess and a hunger for new skills, they're diving headfirst into the React universe. Get ready for some mind-blowing creations and a 😃 that never quits! #PHPProLearningReact</p>
+        </div>
+    )
+}
+
+function Skills(){
+    return (
+        <div className="skills">
+            <Skill skill="Coding 💻"/>
+            <Skill skill="Database 🗄️"/>
+            <Skill skill="Web Development 🌐"/>
+            <Skill skill="Mobile Apps 📱"/>
+            <Skill skill="Problem Solving 🧩"/>
+            <Skill skill="Testing 🧪"/>
+            <Skill skill="DevOps 🚀"/>
+            <Skill skill="Analysis 📊"/>
+            <Skill skill="Version Control 🛠️"/>
+            <Skill skill="Learning 📚"/>
+            <Skill skill="Dragon 🐉"/>
+        </div>
+    )
+}
+
+function Skill(props){
+    return (
+        <div class="skill">
+            <p>{props.skill}</p>
+        </div>
+    )
+}
+
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
